59570bcf823b7ea134c904c93d52bab835275ddb
jvJUCA <jucafranzoni1@gmail.com>
2024-04-27 01:51:14 -0300

    feat(style): mouse events in css and js

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -17,4 +17,28 @@ const Rotas = () => {
      );
 }
 
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link");
+
+window.addEventListener("mousemove", (e) => {
+  
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+      })
+      link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+      })
+    })
+    
+  })
+  
+})
 export default Rotas;
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,10 +5,12 @@ import '../App.css';
 const Home = () => {
   return (
     <div  className="App" style={{marginTop: "20vh"}}>
+      <div class="cursor outer"></div>
+<div class="cursor inner"></div>
       <h1>Página Inicial</h1>
       <header>
       <Stack gap={2} className="mt-5 col-md-5 mx-auto">
-          <Button variant="primary" href="/About">
+          <Button class="button" variant="primary" href="/About">
             About
           </Button>
           <Button href="/User" variant="success">
