c4c77a3409a50e44fbab80b43f4c46fe101f8060
Giovanni <giovanni-nog@hotmail.com>
2024-05-13 22:23:41 -0300

    Update - New Branch (giovanniBranch)

--- src/Views/RegisterForm/RegisterView.jsx
+++ src/Views/RegisterForm/RegisterView.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import "./LoginView.css";
+import "./RegisterView.css";
 import { Link } from 'react-router-dom';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 
-const LoginForm = () => {
+const RegisterForm = () => {
   return (
     <div className="loginbody">
       <div className="cursor"></div>
       <div className="wrapper">
         <form action="">
-          <h1>Login</h1>
+          <h1>Registrar-se</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Nome" required />
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder="Sobrenome" required />
+          </div>
           <div className="input-box">
             <input type="text" placeholder="E-mail" required />
             <FaUser className="icon" />
@@ -19,16 +25,20 @@ const LoginForm = () => {
             <input type="password" placeholder="Senha" required />
             <FaLock className="icon" />
           </div>
-          <div className="remember-forgot">
+          <div className="input-box">
+            <input type="password" placeholder="Confirmar Senha" required />
+            <FaLock className="icon" />
+          </div>
+          {/* <div className="remember-forgot">
             <label>
               <input type="checkbox" /> Mantenha-se conectado
             </label>
             <a href="#">Esqueci minha senha</a>
-          </div>
-          <button type="submit">Login</button>
+          </div> */}
+          <button type="submit">Inscrever-se</button>
           <div className="register-link">
             <p>
-              Não tem uma conta? <Link to="/register">Registre-se</Link><br /> <br /><br />
+              Já tem uma conta? <Link to="/login">Faça Login</Link><br /> <br /><br />
               <Link to="/">Página Inicial</Link>
             </p>
           </div>
@@ -38,4 +48,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
